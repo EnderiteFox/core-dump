9c053ce154a3ddca4dab8f186dd7705d52370c33
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2026-09-24 23:01:35 +0200

    Implement escape menu

--- player/player.gd
+++ player/player.gd
@@ -1,2 +1,10 @@
 class_name Player
 extends CharacterBody3D
+
+
+@onready var menu: EscapeMenu = %EscapeMenu
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"open_menu") and not menu.visible:
+		menu.open()
