8acf1add675c09b615de4604b8d7148c824ecbfa
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2026-09-24 22:59:48 +0200

    Implement lobby player limit
    Fix player connection issues

--- menus/lobby/lobby_menu.gd
+++ menus/lobby/lobby_menu.gd
@@ -3,6 +3,7 @@ extends Control
 
 
 var LOBBY_ROW_SCENE: PackedScene = preload("uid://ctvqdo1he25pt")
+var POPUP_SCENE: PackedScene = preload("uid://c7rtm01au0jfa")
 
 
 @onready var connecting_label: Label = %ConnectingToServerLabel
@@ -17,8 +18,17 @@ var LOBBY_ROW_SCENE: PackedScene = preload("uid://ctvqdo1he25pt")
 @onready var host_menu: GenericPopup = %HostMenu
 
 
+var disconnection_reason: String
+
+
 func _ready() -> void:
+	if not disconnection_reason.is_empty():
+		show_disconnection_popup(disconnection_reason)
+		
 	# Connect to main server
+	var mult: MultiplayerAPI = SceneMultiplayer.new()
+	get_tree().set_multiplayer(mult, get_tree().root.get_path())
+	
 	var peer := ENetMultiplayerPeer.new()
 	peer.create_client("127.0.0.1", MainServer.PORT)
 	self.multiplayer.multiplayer_peer = peer
@@ -27,12 +37,22 @@ func _ready() -> void:
 	self.multiplayer.server_disconnected.connect(_on_server_disconnected)
 	refresh_timer.timeout.connect(_on_lobby_refresh_timeout)
 	MainServer.lobby_list_updated.connect(_on_lobby_list_updated)
-	
+
 	create_lobby_button.pressed.connect(_on_create_lobby_pressed)
 	back_button.pressed.connect(_on_back_pressed)
-	
+
 	host_menu.exiting.connect(_on_host_menu_closed)
 	host_menu.lobby_created.connect(_on_lobby_creation)
+	
+	
+func show_popup(text: String) -> void:
+	var popup: GamePopup = POPUP_SCENE.instantiate()
+	popup.text = text
+	self.add_child(popup)
+	
+	
+func show_disconnection_popup(text: String) -> void:
+	show_popup("Disconnected: " + text)
 	
 	
 func _on_server_connected() -> void:
